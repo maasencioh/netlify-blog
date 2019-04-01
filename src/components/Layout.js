27f945c751db8a41bc88dfe710cfe279ce9094f5
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2019-03-31 22:09:12 -0500

    Cambio de favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -31,11 +31,6 @@ const TemplateWrapper = ({ children }) => {
           sizes="16x16"
         />
 
-        <link
-          rel="mask-icon"
-          href="/img/safari-pinned-tab.svg"
-          color="#ff4400"
-        />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
