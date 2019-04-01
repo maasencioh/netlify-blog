fae75e3723122d9eeece2abe5f5d8a16e93f26a4
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2019-03-31 19:08:09 -0500

    Unify main colors to blue

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -6,6 +6,8 @@ import Layout from '../components/Layout';
 import Features from '../components/Features';
 import BlogRoll from '../components/BlogRoll';
 
+const MAIN_COLOR = '#303F9F'
+
 export const IndexPageTemplate = ({
   image,
   title,
@@ -41,8 +43,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              `${MAIN_COLOR} 0.5rem 0px 0px, ${MAIN_COLOR} -0.5rem 0px 0px`,
+            backgroundColor: MAIN_COLOR,
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -54,8 +56,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
+              `${MAIN_COLOR} 0.5rem 0px 0px, ${MAIN_COLOR} -0.5rem 0px 0px`,
+            backgroundColor: MAIN_COLOR,
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
